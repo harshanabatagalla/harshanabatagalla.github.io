f002bcceda801817a1b19f752e9d0376c575c24a
Harshana Batagalla <67497499+harshanabatagalla@users.noreply.github.com>
2025-03-03 19:16:30 +0530

    feat: add asset imports and images for project display

---
+++ src/assets/assets.js
@@ -0,0 +1,12 @@
+import zelvira from './zelvira.png'
+import umbrellaGo from './umbrellaGO.png'
+import journeyJive from './journey-jive.png'
+import portfolio from './portfolio.png'
+
+export const assets = {
+    zelvira,
+    umbrellaGo,
+    journeyJive,
+    portfolio
+}
+
