d4ac3cba09075032f04111334c3d4072f67a7d4a
Harshana Batagalla <67497499+harshanabatagalla@users.noreply.github.com>
2025-03-03 16:40:47 +0530

    feat: add work experiences data for internship details

---
+++ src/utils/data.js
@@ -0,0 +1,18 @@
+
+  
+  export const workExperiences = [
+    {
+      id: 1,
+      company: "99x",
+      position: "Trainee Software Engineer",
+      duration: "January 2024 - August 2024",
+      description: [
+        "Joined a eight-month internship during my third year as an undergraduate.",
+        "Contributed to the development of new features, including the homepage, and enhancements for the 99x official website, a CMS-based website built using Gatsby.",
+        "Worked on the project portal of the company, a web application developed using Angular, .Net, and EF Core.",
+        "Engaged in both front-end and back-end development of the project portal, gaining professional experience in Angular, .Net, EF Core, and SQL Server.",
+        "Collaborated with cross-functional teams, including designers, product managers, marketing personnel, and other developers, to create high-quality products."
+      ]
+    }
+  ];
+  
