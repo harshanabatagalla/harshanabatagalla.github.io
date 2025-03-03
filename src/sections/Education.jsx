a2d9ba84ed7cad8461750232a19571c322030eeb
Harshana Batagalla <67497499+harshanabatagalla@users.noreply.github.com>
2025-03-03 10:48:22 +0530

    feat: implement folder structure

---
+++ src/sections/Education.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Education = () => {
+    return (
+        <div>
+            <h1>Education</h1>
+        </div>
+    )
+}
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Hero = () => {
+    return (
+        <section>
+            <h1>Hero</h1>
+        </section>
+    );
+}
