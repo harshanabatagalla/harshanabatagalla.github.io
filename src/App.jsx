f37d4b6653515abaa09e7d44ee04795efee56939
Harshana Batagalla <67497499+harshanabatagalla@users.noreply.github.com>
2025-03-14 22:38:50 +0530

    fix: switch from BrowserRouter to HashRouter for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ActivityBar from './components/ActivityBar';
 import TabBar from './components/TabBar';
 import StatusBar from './components/StatusBar';
